f15cb6451b09d3e7892eb648e160b0cc82801247
anuchaj <anuchaj50@gmail.com>
2022-01-18 13:09:45 +0100

    biography

---
+++ scripts/task2.js
@@ -0,0 +1,72 @@
+/* Lesson 2 */
+
+/* VARIABLES */
+
+// Step 1: declare and instantiate a variable to hold your name
+let myName = "Joseph Anucha";
+
+
+// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
+document.querySelector("#name").innerHTML = myName;
+
+
+// Step 3: declare and instantiate a variable to hold the current year
+let currentYear = 2022;
+
+
+// Step 4: place the value of the current year variable into the HTML file
+document.querySelector("#year").innerHTML = currentYear;
+
+
+// Step 5: declare and instantiate a variable to hold the name of your picture
+let myPicture = "images/JAPort.jpg";
+
+
+// Step 6: copy your image into the "images" folder
+
+// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+//document.querySelector('#mypix').setAttribute('src', 'images/JAPort.jpg');
+document.querySelector("img").setAttribute("src", myPicture);
+
+
+
+
+/* ARRAYS */
+
+// Step 1: declare and instantiate an array variable to hold your favorite foods
+let favFoods = ["Akpu", " Rice", " Gari", " Spagh"];
+
+
+// Step 2: place the values of the favorite foods variable into the HTML file
+document.querySelector("#food").innerHTML = favFoods;
+
+
+// Step 3: declare and instantiate a variable to hold another favorite food
+let anotherFavFood = " Beans";
+
+
+// Step 4: add the variable holding another favorite food to the favorite food array
+favFoods.push(anotherFavFood);
+
+
+// Step 5: repeat Step 2
+document.querySelector("#food").innerHTML = favFoods;
+
+
+// Step 6: remove the first element in the favorite foods array
+favFoods.shift();
+
+
+// Step 7: repeat Step 2
+document.querySelector("#food").innerHTML = favFoods;
+
+
+// Step 8: remove the last element in the favorite foods array
+favFoods.pop();
+
+
+// Step 7: repeat Step 2
+document.querySelector("#food").innerHTML = favFoods;
+
+
+
